31e977eddb18d5f2a3901b2a4e88dc3b2e534eae
Kentish7 <50813361+Kentish7@users.noreply.github.com>
2021-06-22 21:48:54 +0000

    Add files via upload

---
+++ profitCalculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This program will give your profit on Crypto!\n";
+    cout << "Type here the amount that you bought: ";
+    double amountBought;
+    cin >> amountBought;
+
+    cout << "Now, type here the price that you bought: ";
+    double priceBought;
+    cin >> priceBought;
+
+    double initialValue = amountBought * priceBought;
+    cout << "Your initial invesment was: " << initialValue <<"!\n";
+
+    cout << "Finally, type here your sell price: ";
+    double sellPrice;
+    cin >> sellPrice;
+
+    cout << "\n";
+    cout << "Your profit is: " << (sellPrice * amountBought) - initialValue << " USD!\n";
+    
+    int currentProfit = (sellPrice - priceBought) / priceBought * 100;
+    cout << "Your profit in percentage is: " << currentProfit << "%!\n";
+    return 0;
+
+    return 0;
